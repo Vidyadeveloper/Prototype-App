b2f957924d16ed889d39f8e0af6dbf6d5a81c2ac
Vidyadeveloper <vidyabharti.sahu@swiftrinity.com>
2025-10-15 11:56:39 +0200

    Update feature-spike-research-conduct-research.js

--- client/src/component/feature-spike-research-conduct-research.js
+++ client/src/component/feature-spike-research-conduct-research.js
@@ -2,7 +2,7 @@
 import {FormDesignerComponent} from "../../../node_modules/@blaze-case-ai/blaze-engine/client/src/component/ux/form-designer-component.js";
 import "../../../node_modules/@blaze-case-ai/blaze-engine/client/src/component/ux/blaze-field-group.js";
 
-class FeatureSpikeResearchConductResearch extends FormDesignerComponent {
+class FeatureSpikeResearchConductResearch extends DynamicHtmlElement {
    constructor() {
     super();
   }
