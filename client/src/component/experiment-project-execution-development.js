26c6dd3834a886eb26bd062818a300fd4111c704
Vidyadeveloper <vidyabharti.sahu@swiftrinity.com>
2025-10-15 11:54:59 +0200

    Update experiment-project-execution-development.js

--- client/src/component/experiment-project-execution-development.js
+++ client/src/component/experiment-project-execution-development.js
@@ -2,7 +2,7 @@
 import {FormDesignerComponent} from "../../../node_modules/@blaze-case-ai/blaze-engine/client/src/component/ux/form-designer-component.js";
 import "../../../node_modules/@blaze-case-ai/blaze-engine/client/src/component/ux/blaze-field-group.js";
 
-class ExperimentProjectExecutionDevelopment extends FormDesignerComponent {
+class ExperimentProjectExecutionDevelopment extends DynamicHtmlElement {
    constructor() {
     super();
   }
