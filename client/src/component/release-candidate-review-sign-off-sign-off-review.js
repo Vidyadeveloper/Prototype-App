c722fbdcdc37053f46d2f87b631a9f5ebc8c7581
Vidyadeveloper <vidyabharti.sahu@swiftrinity.com>
2025-10-15 11:57:21 +0200

    Update release-candidate-review-sign-off-sign-off-review.js

--- client/src/component/release-candidate-review-sign-off-sign-off-review.js
+++ client/src/component/release-candidate-review-sign-off-sign-off-review.js
@@ -2,7 +2,7 @@
 import {FormDesignerComponent} from "../../../node_modules/@blaze-case-ai/blaze-engine/client/src/component/ux/form-designer-component.js";
 import "../../../node_modules/@blaze-case-ai/blaze-engine/client/src/component/ux/blaze-field-group.js";
 
-class ReleaseCandidateReviewSignOffSignOffReview extends FormDesignerComponent {
+class ReleaseCandidateReviewSignOffSignOffReview extends DynamicHtmlElement {
    constructor() {
     super();
   }
