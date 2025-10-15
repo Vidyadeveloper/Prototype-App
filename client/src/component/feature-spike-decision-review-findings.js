d63e66ace0a17f3223409fe2aafee2f2e97c6a3c
Vidyadeveloper <vidyabharti.sahu@swiftrinity.com>
2025-10-15 11:56:14 +0200

    Update feature-spike-decision-review-findings.js

--- client/src/component/feature-spike-decision-review-findings.js
+++ client/src/component/feature-spike-decision-review-findings.js
@@ -2,7 +2,7 @@
 import {FormDesignerComponent} from "../../../node_modules/@blaze-case-ai/blaze-engine/client/src/component/ux/form-designer-component.js";
 import "../../../node_modules/@blaze-case-ai/blaze-engine/client/src/component/ux/blaze-field-group.js";
 
-class FeatureSpikeDecisionReviewFindings extends FormDesignerComponent {
+class FeatureSpikeDecisionReviewFindings extends DynamicHtmlElement {
    constructor() {
     super();
   }
