4b08ba649c0662d0debf71e3d64aa51ec63ec95f
Vidyadeveloper <vidyabharti.sahu@swiftrinity.com>
2025-10-15 11:55:12 +0200

    Update experiment-project-execution-run-spike.js

--- client/src/component/experiment-project-execution-run-spike.js
+++ client/src/component/experiment-project-execution-run-spike.js
@@ -2,7 +2,7 @@
 import {FormDesignerComponent} from "../../../node_modules/@blaze-case-ai/blaze-engine/client/src/component/ux/form-designer-component.js";
 import "../../../node_modules/@blaze-case-ai/blaze-engine/client/src/component/ux/blaze-field-group.js";
 
-class ExperimentProjectExecutionRunSpike extends FormDesignerComponent {
+class ExperimentProjectExecutionRunSpike extends DynamicHtmlElement {
    constructor() {
     super();
   }
