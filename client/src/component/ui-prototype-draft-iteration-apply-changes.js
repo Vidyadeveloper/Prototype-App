30ec7c4358595b52ef5de59f331da84bca24ac3c
Vidyadeveloper <vidyabharti.sahu@swiftrinity.com>
2025-10-15 11:58:10 +0200

    Update ui-prototype-draft-iteration-apply-changes.js

--- client/src/component/ui-prototype-draft-iteration-apply-changes.js
+++ client/src/component/ui-prototype-draft-iteration-apply-changes.js
@@ -2,7 +2,7 @@
 import {FormDesignerComponent} from "../../../node_modules/@blaze-case-ai/blaze-engine/client/src/component/ux/form-designer-component.js";
 import "../../../node_modules/@blaze-case-ai/blaze-engine/client/src/component/ux/blaze-field-group.js";
 
-class UiPrototypeDraftIterationApplyChanges extends FormDesignerComponent {
+class UiPrototypeDraftIterationApplyChanges extends DynamicHtmlElement {
    constructor() {
     super();
   }
