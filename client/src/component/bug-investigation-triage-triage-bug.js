247befd5314edfe64627a56ee0077da93fe54800
Vidyadeveloper <vidyabharti.sahu@swiftrinity.com>
2025-10-15 11:54:34 +0200

    Update bug-investigation-triage-triage-bug.js

--- client/src/component/bug-investigation-triage-triage-bug.js
+++ client/src/component/bug-investigation-triage-triage-bug.js
@@ -2,7 +2,7 @@
 import {FormDesignerComponent} from "../../../node_modules/@blaze-case-ai/blaze-engine/client/src/component/ux/form-designer-component.js";
 import "../../../node_modules/@blaze-case-ai/blaze-engine/client/src/component/ux/blaze-field-group.js";
 
-class BugInvestigationTriageTriageBug extends FormDesignerComponent {
+class BugInvestigationTriageTriageBug extends DynamicHtmlElement {
    constructor() {
     super();
   }
